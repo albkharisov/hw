9889a2a8bb79d90e090a6077dbbb496037ff8eaa
Albert Kharisov <albkharisov@gmail.com>
2022-03-15 18:43:51 +0400

    HW4 is completed

--- hw04_lru_cache/cache.go
+++ hw04_lru_cache/cache.go
@@ -1,23 +1,24 @@
 package hw04lrucache
 
+import "fmt"
+
 type Key string
 
 type Cache interface {
 	Set(key Key, value interface{}) bool
 	Get(key Key) (interface{}, bool)
 	Clear()
+	PrintCache()
 }
 
 type lruCache struct {
-	Cache // Remove me after realization.
-
 	capacity int
 	queue    List
 	items    map[Key]*ListItem
 }
 
 type cacheItem struct {
-	key   string
+	key   Key
 	value interface{}
 }
 
@@ -27,4 +28,52 @@ func NewCache(capacity int) Cache {
 		queue:    NewList(),
 		items:    make(map[Key]*ListItem, capacity),
 	}
+}
+
+func (c *lruCache) Set(key Key, value interface{}) bool {
+	val, ok := c.items[key]
+
+	if !ok {
+		c.items[key] = c.queue.PushFront(cacheItem{key, value})
+
+		if c.queue.Len() > c.capacity {
+			item := c.queue.Back().Value.(cacheItem)
+			delete(c.items, item.key)
+			c.queue.Remove(c.queue.Back())
+		}
+	} else {
+		val.Value = cacheItem{key, value}
+		c.queue.MoveToFront(val)
+	}
+
+	return ok
+}
+
+func (c *lruCache) Get(key Key) (interface{}, bool) {
+	val, ok := c.items[key]
+	if !ok {
+		return nil, false
+	}
+	c.queue.MoveToFront(val)
+	return val.Value.(cacheItem).value, ok
+}
+
+func (c *lruCache) Clear() {
+	c.items = make(map[Key]*ListItem, c.capacity)
+	c.queue.Clear()
+}
+
+/* Used for a debug */
+func (c *lruCache) PrintCache() {
+	fmt.Println("================================================")
+	c.queue.PrintList(
+		func(x interface{}) {
+			fmt.Printf("[%v]=%v ", x.(cacheItem).key, x.(cacheItem).value)
+		})
+	fmt.Printf("\n{ ")
+
+	for k := range c.items {
+		fmt.Printf("%v ", k)
+	}
+	fmt.Printf("}\n")
 }
--- hw04_lru_cache/list.go
+++ hw04_lru_cache/list.go
@@ -1,5 +1,7 @@
 package hw04lrucache
 
+import "fmt"
+
 type List interface {
 	Len() int
 	Front() *ListItem
@@ -8,6 +10,8 @@ type List interface {
 	PushBack(v interface{}) *ListItem
 	Remove(i *ListItem)
 	MoveToFront(i *ListItem)
+	Clear()
+	PrintList(f func(interface{}))
 }
 
 type ListItem struct {
@@ -17,10 +21,102 @@ type ListItem struct {
 }
 
 type list struct {
-	List // Remove me after realization.
-	// Place your code here.
+	length int
+	front  *ListItem
+	back   *ListItem
 }
 
 func NewList() List {
 	return new(list)
+}
+
+func (l list) Len() int {
+	return l.length
+}
+
+func (l list) Front() *ListItem {
+	return l.front
+}
+
+func (l list) Back() *ListItem {
+	return l.back
+}
+
+func (l *list) unlink(li *ListItem) {
+
+	if li.Prev != nil {
+		li.Prev.Next = li.Next
+	} else {
+		l.front = li.Next
+	}
+	if li.Next != nil {
+		li.Next.Prev = li.Prev
+	} else {
+		l.back = li.Prev
+	}
+}
+
+func (l *list) linkToFront(li *ListItem) {
+	li.Next = l.front
+	li.Prev = nil
+	if l.front == nil {
+		l.back = li
+	} else {
+		l.front.Prev = li
+	}
+	l.front = li
+}
+
+func (l *list) linkToBack(li *ListItem) {
+	li.Prev = l.back
+	li.Next = nil
+	if l.back == nil {
+		l.front = li
+	} else {
+		l.back.Next = li
+	}
+	l.back = li
+}
+
+func (l *list) PushFront(v interface{}) *ListItem {
+	li := new(ListItem)
+	li.Value = v
+
+	l.linkToFront(li)
+	l.length++
+	return li
+}
+
+func (l *list) PushBack(v interface{}) *ListItem {
+	li := new(ListItem)
+	li.Value = v
+
+	l.linkToBack(li)
+	l.length++
+	return li
+}
+
+func (l *list) Remove(li *ListItem) {
+	l.unlink(li)
+	l.length--
+}
+
+func (l *list) MoveToFront(li *ListItem) {
+	l.unlink(li)
+	l.linkToFront(li)
+}
+
+func (l *list) Clear() {
+	l.front = nil
+	l.back = nil
+	l.length = 0
+
+	// God save the Garbage Collector!
+}
+
+func (l *list) PrintList(f func(interface{})) {
+	fmt.Printf("list length: %v: ", l.length)
+	for i := l.front; i != nil; i = i.Next {
+		f(i.Value)
+	}
 }
