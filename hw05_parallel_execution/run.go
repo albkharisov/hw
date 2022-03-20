a95f4562384e726fff7b6ece7fe1e80aa57794e7
Albert Kharisov <albkharisov@gmail.com>
2022-03-20 22:56:57 +0400

    HW5 is completed

--- hw05_parallel_execution/run.go
+++ hw05_parallel_execution/run.go
@@ -2,14 +2,84 @@ package hw05parallelexecution
 
 import (
 	"errors"
+	"fmt"
+	"sync"
 )
 
 var ErrErrorsLimitExceeded = errors.New("errors limit exceeded")
 
 type Task func() error
 
+type routineData struct {
+	f   chan Task
+	err chan error
+	wg  sync.WaitGroup
+}
+
+func routine(rd *routineData) {
+	for {
+		if f, ok := <-rd.f; ok {
+			rd.err <- f()
+		} else {
+			break
+		}
+	}
+
+	rd.wg.Done()
+}
+
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
+
+func log(str string) {
+	a := 0
+	if a > 0 {
+		fmt.Println(str)
+	}
+}
+
 // Run starts tasks in n goroutines and stops its work when receiving m errors from tasks.
 func Run(tasks []Task, n, m int) error {
-	// Place your code here.
-	return nil
+	resultErr := ErrErrorsLimitExceeded
+	workersNumber := min(len(tasks), n)
+	rd := routineData{
+		f:   make(chan Task, workersNumber),
+		err: make(chan error, workersNumber),
+	}
+	rd.wg.Add(workersNumber)
+
+	tasksQueued := 0
+	for ; tasksQueued < workersNumber; tasksQueued++ {
+		go routine(&rd)
+		rd.f <- tasks[tasksQueued]
+	}
+
+Exit:
+	for {
+		err := <-rd.err
+		if m >= 0 && err != nil {
+			m--
+			if m == 0 {
+				break Exit
+			}
+		}
+
+		if tasksQueued < len(tasks) {
+			rd.f <- tasks[tasksQueued]
+			tasksQueued++
+		} else {
+			resultErr = nil
+			break Exit
+		}
+	}
+
+	close(rd.f)
+	rd.wg.Wait()
+	close(rd.err)
+
+	return resultErr
 }
