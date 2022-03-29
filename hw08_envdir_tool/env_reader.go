789b2c433c1781bd959cb7ac5eb0e6b3fd04941b
Albert Kharisov <albkharisov@gmail.com>
2022-03-30 02:26:04 +0400

    HW8 is completed

--- hw08_envdir_tool/env_reader.go
+++ hw08_envdir_tool/env_reader.go
@@ -1,5 +1,18 @@
 package main
 
+import (
+	"bufio"
+	"bytes"
+	"fmt"
+	"os"
+	"path/filepath"
+	"strings"
+)
+
+const (
+	MaxEnvVarSize = 512
+)
+
 type Environment map[string]EnvValue
 
 // EnvValue helps to distinguish between empty files and files with the first empty line.
@@ -10,7 +23,51 @@ type EnvValue struct {
 
 // ReadDir reads a specified directory and returns map of env variables.
 // Variables represented as files where filename is name of variable, file first line is a value.
+// Skip files which are > MaxEnvVarSize (512).
 func ReadDir(dir string) (Environment, error) {
-	// Place your code here
-	return nil, nil
+	envmap := make(Environment)
+
+	err := filepath.Walk(dir, func(path string, info os.FileInfo, errWalk error) error {
+		if errWalk != nil || !info.Mode().IsRegular() {
+			if errWalk != nil {
+				fmt.Println(errWalk)
+				return errWalk
+			}
+			return nil
+		}
+
+		if info.Size() > MaxEnvVarSize {
+			return nil
+		}
+
+		file, errOpen := os.Open(path)
+		if errOpen != nil {
+			return nil
+		}
+		defer file.Close()
+
+		var value EnvValue
+		if info.Size() == 0 {
+			value.NeedRemove = true
+		} else {
+			reader := bufio.NewReader(file)
+			line, _, err := reader.ReadLine()
+			if err != nil {
+				return nil
+			}
+
+			line = bytes.ReplaceAll(line, []byte{0x00}, []byte{'\n'})
+			value.NeedRemove = false
+			value.Value = strings.TrimRight(string(line), " \t")
+		}
+		envmap[filepath.Base(path)] = value
+
+		return nil
+	})
+	if err != nil {
+		fmt.Println(err)
+		return nil, err
+	}
+
+	return envmap, nil
 }
--- hw08_envdir_tool/executor.go
+++ hw08_envdir_tool/executor.go
@@ -1,7 +1,59 @@
 package main
 
+import (
+	"errors"
+	"fmt"
+	"os"
+	"os/exec"
+	"syscall"
+)
+
+//
+//func setupEnv(env []string, envMap Environment) []string {
+//
+//	ret := make([]string, len(env))
+//	copy(ret, env)
+//
+//	for i := range envMap {
+//		if envMap[i].NeedRemove {
+//			os.Unsetenv(i)
+//		} else {
+//			ret = append(ret, fmt.Sprintf("%s=%s", i, envMap[i].Value))
+//			//			os.Setenv(i, envMap[i].Value)
+//		}
+//	}
+//
+//	return ret
+//}
+//
+
 // RunCmd runs a command + arguments (cmd) with environment variables from env.
-func RunCmd(cmd []string, env Environment) (returnCode int) {
-	// Place your code here.
-	return
+func RunCmd(cmdLine []string, env Environment) (returnCode int) {
+	//	os.Clearenv()
+
+	for i := range env {
+		if env[i].NeedRemove {
+			os.Unsetenv(i)
+		} else {
+			os.Setenv(i, env[i].Value)
+		}
+	}
+
+	cmd := exec.Command(cmdLine[0], (cmdLine[1:])...)
+
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+
+	if err := cmd.Run(); err != nil {
+		var cmdError *exec.ExitError
+		if errors.As(err, &cmdError) {
+			if status, ok := cmdError.Sys().(syscall.WaitStatus); ok {
+				return status.ExitStatus()
+			}
+		}
+		fmt.Printf("err of cmd.Run(): %v\n", err)
+		return -1
+	}
+
+	return 0
 }
