7b14f1c2a09d0d1c9e7c0b9d4f26644ad436fd09
Albert Kharisov <albkharisov@gmail.com>
2022-04-28 14:32:48 +0400

    HW3, frequency analysis (top 10) (#4)

--- hw03_frequency_analysis/top.go
+++ hw03_frequency_analysis/top.go
@@ -1,6 +1,58 @@
 package hw03frequencyanalysis
 
-func Top10(_ string) []string {
-	// Place your code here.
-	return nil
+import (
+	"sort"
+	"strings"
+)
+
+type pair struct {
+	word  string
+	count int
+}
+
+type pairSlice []pair
+
+func (p pairSlice) Len() int      { return len(p) }
+func (p pairSlice) Swap(i, j int) { p[i], p[j] = p[j], p[i] }
+func (p pairSlice) Less(i, j int) bool {
+	var result bool = (p[i].count < p[j].count) ||
+		((p[i].count == p[j].count) &&
+			(p[i].word > p[j].word))
+	return result
+}
+
+func (p *pairSlice) AddWord(word string) {
+	found := false
+
+	for i := range *p {
+		if (*p)[i].word == word {
+			found = true
+			(*p)[i].count++
+			break
+		}
+	}
+
+	if !found {
+		*p = append(*p, pair{word, 1})
+	}
+}
+
+func Top10(input string) []string {
+	var pl pairSlice
+
+	for _, v := range strings.Fields(input) {
+		pl.AddWord(v)
+	}
+
+	sort.Sort(sort.Reverse(pl))
+
+	result := make([]string, 0, 10)
+	for k := range pl {
+		result = append(result, pl[k].word)
+		if len(result) == 10 {
+			break
+		}
+	}
+
+	return result
 }
